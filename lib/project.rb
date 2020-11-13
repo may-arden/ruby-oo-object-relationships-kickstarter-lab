d3c3501ed78622bc5477af5886e227cb662428a3
may arden <mayard3n@gmail.com>
2020-11-13 02:00:26 -0800

    commit tree 6050e37f6f21ba19d748d543440c6b61c908f938

---
+++ lib/project.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Project
+    attr_reader :title
+
+    def initialize(title)
+        @title = title 
+    end 
+
+    def add_backer(backer)
+        ProjectBacker.new(self, backer)
+    end 
+
+    def backers
+        project_backer_instances = ProjectBacker.all.select do |project_backer|
+            project_backer.project == self
+        end
+
+        project_backer_instances.map do |pbi|
+            pbi.backer
+        end
+    end
+
+end 
